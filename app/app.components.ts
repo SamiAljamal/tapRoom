eb4155bbf045e54eca01b2299905b2cacb351a93
Bhawani Parajuli and Sami Al-Jamal <sami@>
2016-08-09 10:34:58 -0700

    Setup project.

--- app/app.components.ts
+++ app/app.components.ts
@@ -2,9 +2,27 @@ import { Component } from 'angular2/core';
 @Component({
   selector: 'my-app',
   template: `
-  <h1>Skeleton Angular2 App!</h1>
+  <div class = "container">
+    <h1>Keg List</h1>
+    <ul *ngFor="#tap of tapRoom">
+    <li><h3> Name: {{tap.name}}</h3><ul><h4><li> Brand: {{tap.brand}}</li><li>Price: {{tap.price}}</li><li>ABV: {{tap.abv}}%</li></h4></ul></li>
+    </ul>
+
   `
 })
 export class AppComponent{
+  public tapRoom: TapRoom[];
+  constructor(){
+    this.tapRoom = [
+      new TapRoom("IPA", "Bud", 6, 10, 0),
+      new TapRoom("Miller Draft", "Miller", 6, 7, 1)
+    ];
+  }
+}
 
+export class TapRoom {
+  public tapEmpty: boolean = false;
+  constructor(public name: string, public brand: string, public price: number, public abv: number, public id: number) {
+
+  }
 }
